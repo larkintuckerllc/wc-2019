771affad517db7ccf6a7377af054e68e8dc2cf51
John Tucker <jtucker@appirio.com>
2019-06-29 13:51:30 -0400

    counter

--- dist/index.js
+++ dist/index.js
@@ -17,12 +17,40 @@ var __extends = (this && this.__extends) || (function () {
         __extends(HelloWorld, _super);
         function HelloWorld() {
             var _this = _super.call(this) || this;
+            _this.buttonEl = null;
+            _this.value = 0;
+            _this.valueEl = null;
+            _this.handleClick = function () {
+                if (_this.valueEl === null) {
+                    return;
+                }
+                _this.value += 1;
+                _this.valueEl.textContent = _this.value.toString();
+            };
             var shadow = _this.attachShadow({ mode: 'closed' });
+            var styleEl = document.createElement('style');
+            styleEl.textContent = "\n      #root {\n        text-align: center;\n      }\n      ";
             var rootEl = document.createElement('div');
-            rootEl.textContent = 'Hello World';
+            rootEl.id = 'root';
+            _this.valueEl = document.createElement('div');
+            _this.valueEl.id = 'root__value';
+            _this.valueEl.textContent = _this.value.toString();
+            _this.buttonEl = document.createElement('button');
+            _this.buttonEl.id = 'root__button';
+            _this.buttonEl.textContent = 'Increment';
+            _this.buttonEl.addEventListener('click', _this.handleClick);
+            shadow.appendChild(styleEl);
+            rootEl.appendChild(_this.valueEl);
+            rootEl.appendChild(_this.buttonEl);
             shadow.appendChild(rootEl);
             return _this;
         }
+        HelloWorld.prototype.disconnectedCallback = function () {
+            if (this.buttonEl === null) {
+                return;
+            }
+            this.buttonEl.removeEventListener('click', this.handleClick);
+        };
         return HelloWorld;
     }(HTMLElement));
     window.customElements.define('hello-world', HelloWorld);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,47 @@
 (() => {
   class HelloWorld extends HTMLElement {
+    private buttonEl: HTMLElement | null = null;
+    private value = 0;
+    private valueEl: HTMLElement | null = null;
+
     constructor() {
       super();
       const shadow = this.attachShadow({ mode: 'closed' });
+      const styleEl = document.createElement('style');
+      styleEl.textContent = `
+      #root {
+        text-align: center;
+      }
+      `;
       const rootEl = document.createElement('div');
-      rootEl.textContent = 'Hello World';
+      rootEl.id = 'root';
+      this.valueEl = document.createElement('div');
+      this.valueEl.id = 'root__value';
+      this.valueEl.textContent = this.value.toString();
+      this.buttonEl = document.createElement('button');
+      this.buttonEl.id = 'root__button';
+      this.buttonEl.textContent = 'Increment';
+      this.buttonEl.addEventListener('click', this.handleClick);
+      shadow.appendChild(styleEl);
+      rootEl.appendChild(this.valueEl);
+      rootEl.appendChild(this.buttonEl);
       shadow.appendChild(rootEl);
     }
+
+    public disconnectedCallback() {
+      if (this.buttonEl === null) {
+        return;
+      }
+      this.buttonEl.removeEventListener('click', this.handleClick);
+    }
+
+    private handleClick = () => {
+      if (this.valueEl === null) {
+        return;
+      }
+      this.value += 1;
+      this.valueEl.textContent = this.value.toString();
+    };
   }
   window.customElements.define('hello-world', HelloWorld);
 })();
