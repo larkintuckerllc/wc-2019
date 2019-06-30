0685ebc6efe2262f85ab18c7f7b7999f38e461d4
John Tucker <jtucker@appirio.com>
2019-06-30 04:48:43 -0400

    remove iife

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,38 @@
 import template from './template.html';
 
-(() => {
-  const templateEl = document.createElement('template');
-  templateEl.innerHTML = template;
-  class HelloWorld extends HTMLElement {
-    private buttonEl: HTMLElement | null = null;
-    private value = 0;
-    private valueEl: HTMLElement | null = null;
+const templateEl = document.createElement('template');
+templateEl.innerHTML = template;
+class HelloWorld extends HTMLElement {
+  private buttonEl: HTMLElement | null = null;
+  private value = 0;
+  private valueEl: HTMLElement | null = null;
 
-    constructor() {
-      super();
-      const shadow = this.attachShadow({ mode: 'closed' });
-      shadow.appendChild(templateEl.content.cloneNode(true));
-      this.valueEl = shadow.getElementById('root__value');
-      this.buttonEl = shadow.getElementById('root__button');
-      if (this.valueEl === null || this.buttonEl === null) {
-        return;
-      }
-      this.valueEl.textContent = this.value.toString();
-      this.buttonEl.addEventListener('click', this.handleClick);
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: 'closed' });
+    shadow.appendChild(templateEl.content.cloneNode(true));
+    this.valueEl = shadow.getElementById('root__value');
+    this.buttonEl = shadow.getElementById('root__button');
+    if (this.valueEl === null || this.buttonEl === null) {
+      return;
     }
-
-    public disconnectedCallback() {
-      if (this.buttonEl === null) {
-        return;
-      }
-      this.buttonEl.removeEventListener('click', this.handleClick);
-    }
-
-    private handleClick = () => {
-      if (this.valueEl === null) {
-        return;
-      }
-      this.value += 1;
-      this.valueEl.textContent = this.value.toString();
-    };
+    this.valueEl.textContent = this.value.toString();
+    this.buttonEl.addEventListener('click', this.handleClick);
   }
-  window.customElements.define('hello-world', HelloWorld);
-})();
+
+  public disconnectedCallback() {
+    if (this.buttonEl === null) {
+      return;
+    }
+    this.buttonEl.removeEventListener('click', this.handleClick);
+  }
+
+  private handleClick = () => {
+    if (this.valueEl === null) {
+      return;
+    }
+    this.value += 1;
+    this.valueEl.textContent = this.value.toString();
+  };
+}
+window.customElements.define('hello-world', HelloWorld);
