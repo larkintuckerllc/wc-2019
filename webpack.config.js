24f25caa64ca86d6b12bdb07c5eef75148e7a550
Arthur Clemens <arthurclemens@gmail.com>
2020-09-27 14:21:31 +0200

    Update webpack.config.js
    Update parameters:
    * html-loader
    * CopyPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = env => {
-  return ({
+module.exports = (env) => {
+  return {
     mode: env.MODE,
     entry: './src/index.ts',
     devtool: env.MODE === 'development' ? 'inline-source-map' : 'none',
     devServer: {
-      contentBase: './dist'
+      contentBase: './dist',
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.(html)$/,
           use: {
             loader: 'html-loader',
             options: {
-              attrs: false,
+              attributes: false,
             },
           },
         },
       ],
     },
     resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ]
+      extensions: ['.tsx', '.ts', '.js'],
     },
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
       new CleanWebpackPlugin(),
-      new CopyPlugin([
-        { from: 'node_modules/@webcomponents/webcomponentsjs', to: 'webcomponentsjs' },
-      ]),
+      new CopyPlugin({
+        patterns: [{ from: 'node_modules/@webcomponents/webcomponentsjs', to: 'webcomponentsjs' }],
+      }),
       new HtmlWebpackPlugin({
         inject: 'head',
-        template: 'public/index.html'
-      })
-    ]
-  });
+        template: 'public/index.html',
+      }),
+    ],
+  };
 };
